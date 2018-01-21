e13dc96d6c75894105c80cd57e1bc157eb75fe24
André N. Darcie <andrendarcie@gmail.com>
2018-01-21 16:56:56 -0200

    Change form name to AbpGenerator

--- AbpGenerator/AbpGeneratorForm.Designer.cs
+++ AbpGenerator/AbpGeneratorForm.Designer.cs
@@ -367,7 +367,7 @@
             this.ClientSize = new System.Drawing.Size(666, 511);
             this.Controls.Add(this.Manager);
             this.Name = "AbpBackEndGeneratorForm";
-            this.Text = "Form1";
+            this.Text = "AbpGenerator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Manager.ResumeLayout(false);
             this.Entidade.ResumeLayout(false);
